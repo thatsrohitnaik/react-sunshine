ada77d72d61c8afde0ee849e6b4abc2a4e4b63c6
thatsrohitnaik <rohit24naik@gmail.com>
2021-05-29 16:53:30 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -5,27 +5,12 @@ import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 import axios from 'axios';
 import Table from './components/table/';
 import schema from './table-schema/table-one';
+import rowData from './table-data/table-one';
 
 import './style.css';
 
-const rowData2 = [
-  {
-    fileName: '1.msg',
-    attachment: false,
-    sourceFile: '3.msg',
-    ext: '.msg',
-    dsEntities: 4,
-    pages: 1,
-    redactEntities: 3,
-    redactEntitiesPages: 2,
-    fromToCcDS: true,
-    redact: false,
-    share: 'Share With DS'
-  }
-];
-
 const App = () => {
-  return <>{rowData2 ? <Table schema={schema} rowData={rowData2} /> : null}</>;
+  return <>{rowData ? <Table schema={schema} rowData={rowData} /> : null}</>;
 };
 
 export default App;
---
+++ src/table-data/table-one.js
@@ -0,0 +1,17 @@
+const rowData = [
+  {
+    fileName: '1.msg',
+    attachment: false,
+    sourceFile: '3.msg',
+    ext: '.msg',
+    dsEntities: 4,
+    pages: 1,
+    redactEntities: 3,
+    redactEntitiesPages: 2,
+    fromToCcDS: true,
+    redact: false,
+    share: 'Share With DS'
+  }
+];
+
+export const rowData;
