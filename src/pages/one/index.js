c615da39275e06b9a9eff6b6dc38cdf362d30774
thatsrohitnaik <rohit24naik@gmail.com>
2021-05-30 23:06:21 +0530

    1 file modified

--- src/pages/one/index.js
+++ src/pages/one/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 import Table from '../../components/table/';
-import schema from './table-schema/table-one';
-import rowData from './table-data/table-one';
+import detailSchema from './table-schema/table-one';
+import detailData from './table-data/table-one';
 import Stepper from '../../components/stepper/';
 import Steps from '../../components/stepper/stepper-one';
 import Panel from '../../components/panel/';
 import NavBar from '../../components/nav/';
 import BasicTable from '../../components/table/basic';
 import infoSchema from './table-schema/table-info';
+import infoData from './table-data/table-info';
+
 import './style.css';
 
 const PageOne = () => {
@@ -35,7 +37,7 @@ const PageOne = () => {
       </div>
       <Stepper steps={Steps} />
       <Panel />
-      {rowData ? <Table schema={schema} rowData={rowData} /> : null}
+      {detailData ? <Table schema={detailSchema} rowData={detailData} /> : null}
     </div>
   );
 };
